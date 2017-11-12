c0f691a7db5f6790f712c7e8d0fbc901c5736856
James Kolce <contact@jameskolce.com>
2017-11-11 22:40:41 -0700

    [fix: #3] Add -w option to watch for file changes

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 #!/usr/bin/env node
 
 const cliParse = require('cliparse');
-const compile = require('@concisecss/preprocessor').default;
+const chokidar = require('chokidar');
+const path = require('path');
+const preprocessor = require('@concisecss/preprocessor').default;
 
 const pkg = require('./package.json');
 const fs = require('fs');
+
+const compile = (input, output, watch) => {
+  // Write compiled file
+  fs.writeFile(output, preprocessor(input), (err) => {
+    if (err) { throw err; }
+
+    console.log(`File written (${new Date(Date.now()).toLocaleString()}): ${output}\nFrom: ${input}`);
+  });
+
+  // Watch for changes if the -w option is set
+  if (watch) {
+    chokidar.watch(`${path.dirname(input)}/**/*.ccss`).on('all', (event, path) => {
+      fs.writeFile(output, preprocessor(input), (err) => {
+        if (err) { throw err; }
+
+        console.log(`File written (${new Date(Date.now()).toLocaleString()}): ${output}\nFrom: ${input}`);
+      });
+    });
+  }
+};
+
+
 
 cliParse.parse(cliParse.cli({
   name: 'concise-cli',
@@ -17,14 +41,15 @@ cliParse.parse(cliParse.cli({
         args: [
           cliParse.argument('input', { description: 'File to compile' }),
           cliParse.argument('output', { description: 'Output CSS file' })
+        ],
+        options: [
+          cliParse.flag('watch', {
+            aliases: ['w'],
+            description: 'Watch for file changes'
+          })
         ]
-      }, params => {
-        fs.writeFile(params.args[1], compile(params.args[0]), (err) => {
-          if (err) { throw err; }
-
-          console.log(`File written: ${params.args[1]}\nFrom: ${params.args[0]}`);
-        })
-      }
+      },
+      params => compile(params.args[0], params.args[1], params.options.watch)
     )
   ]
 }));
